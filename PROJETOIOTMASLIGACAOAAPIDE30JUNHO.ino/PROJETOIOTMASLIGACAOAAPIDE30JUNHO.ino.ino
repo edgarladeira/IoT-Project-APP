45733a7131414f7f84fce556f45bd7c4cfa08281
beatriz <beatrizcaldeira01@gmail.com>
2023-06-01 18:53:29 +0100

    FINAL

--- PROJETOIOTMASLIGACAOAAPIDE30JUNHO.ino/PROJETOIOTMASLIGACAOAAPIDE30JUNHO.ino.ino
+++ PROJETOIOTMASLIGACAOAAPIDE30JUNHO.ino/PROJETOIOTMASLIGACAOAAPIDE30JUNHO.ino.ino
@@ -1,27 +1,41 @@
 #include <Stepper.h> //INCLUSÃO DE BIBLIOTECA
-#define MAX_JSON_LENGTH 200
-
 #include <SPI.h>
 #include <Ethernet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dht.h>
 
+
+#define MAX_JSON_LENGTH 200
+
+
+//#####################
+// MOTOR DE REGA
 const int stepsPerRevolution = 100; //NÚMERO DE PASSOS POR VOLTA
- 
-const int ldr = A0;
-int valorldr = 0;
-
-//Arduino - Sensor de humidade do solo e Sensor de chuva
-
-int sensorHumidadeSolo = A1;   // Sensor de humidade pino A0 conectado no A0 do Arduino
-int valorLimiteHumidade = 500; // valor da tensão de comparação (valor máximo = 1024)
-bool soloHumido;               // condição de solo Húmido 
-
 Stepper myStepper(stepsPerRevolution, 8,10,9,11); //INICIALIZA O MOTOR UTILIZANDO OS PINOS DIGITAIS 8, 9, 10, 11
 
-int DHpin = 12; // input/output pin
-byte dat[5];   
+ 
+//#####################
+// SENSOR DE LUZ 
+const int ldr = A0;
+int valorldr = 0;
+int valorMaximoDeLDR = 500;
+
+//#####################
+// SENSOR DE HUMIDADE NO SOLO
+
+int sensorHumidadeSolo = A1;   // Sensor de humidade pino A0 conectado no A0 do Arduino
+int valorLimiteHumidade = 600; // valor da tensão de comparação (valor máximo = 1024)
+bool soloHumido;               // condição de solo Húmido 
+
+
+int DHT11 = A2; // input/output pin
+int U;   // sensor de umidade
+int T;   
+dht sensor;
+
+
 
 char server[] = "192.168.0.112:8080";    // name address for Google (using DNS)
 
@@ -43,46 +57,6 @@ unsigned long byteCount = 0;
 bool printWebData = true;  // set to false for better speed measurement
 
 
-//float tempAr = 22;
-//float humSolo = 80;
-//float luz = 500;
-//bool maduro = false;
-//float humAr = 90;
-
-
-
-byte read_data()
-{
-  byte i = 0;
-  byte result = 0;
-  for (i = 0; i < 8; i++) {
-      while (digitalRead(DHpin) == LOW); // wait 50us
-      delayMicroseconds(30); //The duration of the high level is judged to determine whether the data is '0' or '1'
-      if (digitalRead(DHpin) == HIGH)
-        result |= (1 << (8 - i)); //High in the former, low in the post
-    while (digitalRead(DHpin) == HIGH); //Data '1', waiting for the next bit of reception
-    }
-  return result;
-}
-void start_test () {
-  digitalWrite (DHpin, LOW); // bus down, send start signal
-  delay (30); // delay greater than 18ms, so DHT11 start signal can be detected
- 
-  digitalWrite (DHpin, HIGH);
-  delayMicroseconds (40); // Wait for DHT11 response
- 
-  pinMode (DHpin, INPUT);
-  if (digitalRead (DHpin) == HIGH);
-    delayMicroseconds (80); // DHT11 response, pulled the bus 80us
-  if (digitalRead (DHpin) == LOW);
-    delayMicroseconds (80); // DHT11 80us after the bus pulled to start sending data
- 
-  for (int i = 0; i < 4; i ++) // receive temperature and humidity data, the parity bit is not considered
-    dat[i] = read_data ();
- 
-  pinMode (DHpin, OUTPUT);
-  digitalWrite (DHpin, HIGH); // send data once after releasing the bus, wait for the host to open the next Start signal
-}
 
 float SensorDeHumidade () {
  int valorSensorHumidadeSolo = analogRead(sensorHumidadeSolo); // faz a leitura do Sensor
@@ -101,15 +75,18 @@ float SensorDeHumidade () {
 
 
 void setup(){
+
   Serial.begin(9600);
     while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   myStepper.setSpeed(300); //VELOCIDADE DO MOTOR
   pinMode(ldr, INPUT);
-  pinMode(DHpin, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(sensorHumidadeSolo, INPUT); // Sensor de humidade - porta A0 é entrada
+
+  pinMode(12, OUTPUT); //LED
+
 
   // start the Ethernet connection:
   Serial.println("Initialize Ethernet with DHCP:");
@@ -146,33 +123,32 @@ void loop(){ //sensor de luminancia
   Serial.print("Valor lido pelo LDR= ");
   Serial.print(valorldr);
 
+
+   if(valorldr < valorMaximoDeLDR){
+     //Ligamos o led
+    digitalWrite(12,HIGH);
+
+  }
+  else{
+    //Desligamos o LED
+  digitalWrite(12,LOW);
+  }
+
+
   //Sensor de humidade/temperatura no ambiente -------- VERR e tratar humidade e temperatura para enviar
 
-  //start_test();
+  sensor.read11(DHT11);
+  U = sensor.humidity;
+  T = sensor.temperature; 
+
   Serial.print("| Humdity = ");
-  Serial.print(dat[0], DEC); //Displays the integer bits of humidity;
-  Serial.print('.');
-  Serial.print(dat[1], DEC); //Displays the decimal places of the humidity;
+  Serial.print(U); //Displays the integer bits of humidity;
   Serial.print('%');
   Serial.print("Temperature = ");
-  Serial.print(dat[2], DEC); //Displays the integer bits of temperature;
-  Serial.print('.');
-  Serial.print(dat[3], DEC); //Displays the decimal places of the temperature;
+  Serial.print(T); //Displays the integer bits of temperature;
   Serial.println('C');
-  byte checksum = dat[0] + dat[1] + dat[2] + dat[3];
-  if (dat[4] != checksum) 
-    Serial.println("-- Checksum Error!");
-  else
-    Serial.println("-- OK");
-  digitalWrite(13, HIGH);
+  int humSoloo = (int) SensorDeHumidade();
   
-  //Sensor de Humidade no Solo
-  float valorhumsolo = 0.0;
-
- // contagem de 1 a 4320 ( 4320 x 5 segundos = 6 horas)
-   valorhumsolo = SensorDeHumidade(); 
-   
-
 
  if (client.connect(server2, 8080)) {
     Serial.print("connected to ");
@@ -180,17 +156,10 @@ void loop(){ //sensor de luminancia
 
     char json[MAX_JSON_LENGTH];
 
-
-    float temperature = 12.5;
     char tempAr[10];
-    float humArf = 22.5;
     char humAr[10];   
-    float humSolof = 22.5;
-
     char humSolo[10];
-
     char luz[10];
-    
     char maduro[10];
     char m[] = "false";
 
@@ -200,9 +169,9 @@ void loop(){ //sensor de luminancia
     // Converter os valores inteiros para strings
     //snprintf(tempAr, sizeof(tempAr), "%f", 12.5);
     // Converter os valores inteiros para strings
-    snprintf(tempAr, sizeof(tempAr), "%d", (int)temperature);
-    snprintf(humAr, sizeof(humAr), "%d", (int)humArf);
-    snprintf(humSolo, sizeof(humSolo), "%d", (int)humSolof);
+    snprintf(tempAr, sizeof(tempAr), "%d", (int)T);
+    snprintf(humAr, sizeof(humAr), "%d", (int)U);
+    snprintf(humSolo, sizeof(humSolo), "%d", humSoloo);
     snprintf(luz, sizeof(luz), "%d", valorldr);
 
 
@@ -240,13 +209,13 @@ void loop(){ //sensor de luminancia
     //while(soloHumido == 0)
     //myStepper.step(stepsPerRevolution); //GIRA O MOTOR NO SENTIDO ANTI-HORÁRIO
 
-    for(int i = 0; i < 50; i++){ //PARA "i" IGUAL A 0, ENQUANTO "i" MENOR QUE 50 INCREMENTA "i"
+    /*for(int i = 0; i < 50; i++){ //PARA "i" IGUAL A 0, ENQUANTO "i" MENOR QUE 50 INCREMENTA "i"
           myStepper.step(stepsPerRevolution); //GIRA O MOTOR NO SENTIDO ANTI-HORÁRIO
   }
    //LAÇO "for" QUE LIMITA O TEMPO EM QUE O MOTOR GIRA NO SENTIDO HORÁRIO
   for(int i = 0; i < 50; i++){//PARA "i" IGUAL A 0, ENQUANTO "i" MENOR QUE 50 INCREMENTA "i"
           myStepper.step(-stepsPerRevolution); //GIRA O MOTOR NO SENTIDO HORÁRIO
-  }
+  }*/
 
   } else {
     // if you didn't get a connection to the server:
